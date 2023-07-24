2ec76908712d274f5007e373df45b19e60b294d5
Kuya Pijay <53463564+yopijay@users.noreply.github.com>
2023-07-24 13:57:41 +0800

    Company
    - Listing
       > Code structure

--- client/src/layouts/pages/maintenance/company/screens/mobile/index.jsx
+++ client/src/layouts/pages/maintenance/company/screens/mobile/index.jsx
@@ -2,36 +2,17 @@
 import { Stack } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 
-// Core
-import { ListPrvdr } from "core/context/List"; // Provider
-
 // Components
-import Title from "../../components/list/Title";
-import Search from "../../components/list/Search";
-import Sort from "../../components/list/Sort";
-import Items from "../../components/list/Items";
+import List from "./List";
 
 // Constants
-import { container, content, items } from "./index.style"; // Style
+import { container } from "./index.style"; // Style
 
 const Index = () => {
     return (
         <Stack sx= { container }>
             <Routes>
-                <Route exact path= "/" element= {
-                    <ListPrvdr>
-                        <Stack sx= { content } spacing= { 5 }>
-                            <Title />
-                            <Stack direction= "column" justifyContent= "flex-start" alignItems= "stretch" spacing= { 2 } flexGrow= { 1 }>
-                                <Search />
-                                <Stack sx= { items } spacing= { 1 }>
-                                    <Sort />
-                                    <Items />
-                                </Stack>
-                            </Stack>
-                        </Stack>
-                    </ListPrvdr>
-                } />
+                <Route exact path= "/" element= { <List /> } />
                 <Route exact path= "/form/:type" element= { 'CREATE' } />
                 <Route exact path= "/form/:type/:id" element= { 'UPDATE' } />
             </Routes>
