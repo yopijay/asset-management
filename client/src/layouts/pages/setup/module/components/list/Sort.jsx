76bcc27247e1b457cf7de78a8f9fe0cea2e8d3d2
Kuya Pijay <53463564+yopijay@users.noreply.github.com>
2023-08-07 09:16:44 +0800

    Update Sort.jsx

--- client/src/layouts/pages/setup/module/components/list/Sort.jsx
+++ client/src/layouts/pages/setup/module/components/list/Sort.jsx
@@ -1,32 +1,37 @@
 // Libraries
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Stack, Typography } from "@mui/material";
 
 // Core
 import { ListCntxt } from "core/context/List"; // Context
+import { FormCntxt } from "core/context/Form"; // Context
 
 // Constants
 import { orderby } from "./list.style"; // Styles
-import { FormCntxt } from "core/context/Form";
 
 const Sort = ({ refetch }) => {
     const { sort, setsort, listing, setlisting } = useContext(ListCntxt);
     const { getValues, setValue } = useContext(FormCntxt);
+    const [ order, setorder ] = useState('date_created');
+
+    const onclick = name => { setValue('orderby', name); setorder(name); refetch({ table: 'tbl_module', data: getValues() }); }
 
     return (
         <Stack direction= "row" justifyContent= "flex-end" alignItems= "center" spacing= { 1 }>
             <Stack direction= "row" justifyContent= "flex-start" alignItems= "center" spacing= { 1 }>
                 <Typography variant= "caption">Order by:</Typography>
-                <Typography variant= "caption" sx= { orderby }>Date created</Typography>
+                { order === 'date_created' ? <Typography variant= "caption" sx= { orderby } onClick= { () => onclick('name') }>Date created</Typography> :
+                    order === 'name' ? <Typography variant= "caption" sx= { orderby } onClick= { () => onclick('series_no') }>Name</Typography> :
+                        order === 'series_no' ? <Typography variant= "caption" sx= { orderby } onClick= { () => onclick('date_created') }>Series no.</Typography> : '' }
             </Stack>
             { sort === 'desc' ? 
                 <Typography sx= {{ cursor: 'pointer' }} 
-                    onClick= { () => { setsort('asc'); setValue('sort', 'asc'); refetch({ table: 'tbl_company', data: getValues() }); } }>
+                    onClick= { () => { setsort('asc'); setValue('sort', 'asc'); refetch({ table: 'tbl_module', data: getValues() }); } }>
                     <FontAwesomeIcon icon= { solid('arrow-down-z-a') } color= "#9DB2BF" /></Typography> :
                 <Typography sx= {{ cursor: 'pointer' }} 
-                    onClick= { () => { setsort('desc'); setValue('sort', 'desc'); refetch({ table: 'tbl_company', data: getValues() }); } }>
+                    onClick= { () => { setsort('desc'); setValue('sort', 'desc'); refetch({ table: 'tbl_module', data: getValues() }); } }>
                     <FontAwesomeIcon icon= { solid('arrow-down-a-z') } color= "#9DB2BF" /></Typography> }
             { listing === 'list' ?
                 <Typography sx= {{ cursor: 'pointer' }} onClick= { () => setlisting('grid') }><FontAwesomeIcon icon= { solid('grip') } color= "#9DB2BF" /></Typography> :
