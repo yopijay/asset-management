caed6d006ee28f7ecc8a62ce654606390e0b8659
Kuya Pijay <53463564+yopijay@users.noreply.github.com>
2023-08-23 10:16:06 +0800

    Classification
    - UI updates

--- client/src/layouts/pages/assets-supplies/classification/components/form/index.jsx
+++ client/src/layouts/pages/assets-supplies/classification/components/form/index.jsx
@@ -55,7 +55,7 @@ const Index = () => {
                     malesuada quam ut, vulputate massa.</Typography>
             </Stack>
             <Stack sx= { card }><FormBuilder fields= { Classification({ fetching: isFetching }) } /></Stack>
-            <Stack direction= "row" justifyContent= "flex-end" alignItems= "center" spacing= { 1 }>
+            <Stack direction= "row" justifyContent= {{ xs: 'space-between', sm: 'flex-end' }} alignItems= "center" spacing= { 1 }>
                 <Typography sx= { cancelbtn } component= { Link } to= "/assets-supplies/classification">Cancel</Typography>
                 { type !== 'view' ? <Typography sx= { savebtn } onClick= { handleSubmit(data => {
                     let errors = [];
