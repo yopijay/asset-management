8c0a690b6edf5e9f1881d6d6e52755f5305c7d1d
Pijay <53463564+yopijay@users.noreply.github.com>
2023-08-29 08:15:52 +0800

    Update public.sql

--- database/public.sql
+++ database/public.sql
@@ -1,18 +1,18 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : kc
+ Source Server         : local
  Source Server Type    : PostgreSQL
- Source Server Version : 140003
+ Source Server Version : 150001
  Source Host           : localhost:5432
- Source Catalog        : db_kc_ams
+ Source Catalog        : db_ams
  Source Schema         : public
 
  Target Server Type    : PostgreSQL
- Target Server Version : 140003
+ Target Server Version : 150001
  File Encoding         : 65001
 
- Date: 25/08/2023 16:57:57
+ Date: 29/08/2023 08:15:27
 */
 
 
@@ -21,6 +21,17 @@
 -- ----------------------------
 DROP SEQUENCE IF EXISTS "public"."tbl_assets_id_seq";
 CREATE SEQUENCE "public"."tbl_assets_id_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 2147483647
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for tbl_assets_info_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."tbl_assets_info_id_seq";
+CREATE SEQUENCE "public"."tbl_assets_info_id_seq" 
 INCREMENT 1
 MINVALUE  1
 MAXVALUE 2147483647
@@ -151,6 +162,42 @@ CREATE TABLE "public"."tbl_assets" (
 -- ----------------------------
 
 -- ----------------------------
+-- Table structure for tbl_assets_info
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."tbl_assets_info";
+CREATE TABLE "public"."tbl_assets_info" (
+  "id" int4 NOT NULL DEFAULT nextval('tbl_assets_info_id_seq'::regclass),
+  "assets_id" int4,
+  "type" varchar(50) COLLATE "pg_catalog"."default",
+  "brand_id" int4,
+  "model" varchar(100) COLLATE "pg_catalog"."default",
+  "panel" varchar(100) COLLATE "pg_catalog"."default",
+  "resolution" varchar(100) COLLATE "pg_catalog"."default",
+  "color" varchar(100) COLLATE "pg_catalog"."default",
+  "cpu" varchar(100) COLLATE "pg_catalog"."default",
+  "gpu" varchar(100) COLLATE "pg_catalog"."default",
+  "hdd" varchar(50) COLLATE "pg_catalog"."default",
+  "ssd" varchar(50) COLLATE "pg_catalog"."default",
+  "ram" varchar(50) COLLATE "pg_catalog"."default",
+  "os" varchar(150) COLLATE "pg_catalog"."default",
+  "power_supply" varchar(70) COLLATE "pg_catalog"."default",
+  "warranty" varchar(100) COLLATE "pg_catalog"."default",
+  "hdmi" varchar(10) COLLATE "pg_catalog"."default",
+  "vga" varchar(10) COLLATE "pg_catalog"."default",
+  "dvi" varchar(10) COLLATE "pg_catalog"."default",
+  "wifi" varchar(10) COLLATE "pg_catalog"."default",
+  "bluetooth" varchar(10) COLLATE "pg_catalog"."default",
+  "fingerprint" varchar(10) COLLATE "pg_catalog"."default",
+  "webcam" varchar(10) COLLATE "pg_catalog"."default",
+  "backlit_keyboard" varchar(10) COLLATE "pg_catalog"."default"
+)
+;
+
+-- ----------------------------
+-- Records of tbl_assets_info
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for tbl_audit_trail
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."tbl_audit_trail";
@@ -171,81 +218,6 @@ CREATE TABLE "public"."tbl_audit_trail" (
 -- ----------------------------
 -- Records of tbl_audit_trail
 -- ----------------------------
-INSERT INTO "public"."tbl_audit_trail" VALUES (1, '534VB8G', 'tbl_company', 1, 'all', NULL, NULL, 'create', 1, '2023-08-02 15:00:29+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (2, 'JGWISHP', 'tbl_company', 2, 'all', NULL, NULL, 'create', 1, '2023-08-02 16:38:22+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (3, 'XTBJ2QL', 'tbl_company', 2, 'status', '1', '0', 'update', 1, '2023-08-03 16:56:38+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (4, 'FR68V5B', 'tbl_company', 2, 'status', '0', '1', 'update', 1, '2023-08-03 16:56:43+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (5, 'T6WIV1U', 'tbl_company', 2, 'address', NULL, '#82 CORDILLERA ST. CORNER QUEZON AVE., QUEZON CITY', 'update', 1, '2023-08-03 16:58:40+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (6, 'I4PS89Y', 'tbl_company', 3, 'all', NULL, NULL, 'create', 1, '2023-08-04 09:55:41+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (7, 'IKSHBG0', 'tbl_module', 1, 'all', NULL, NULL, 'create', 1, '2023-08-04 16:32:03+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (8, 'HZGNAQ1', 'tbl_module', 1, 'description', NULL, 'TESTING', 'update', 1, '2023-08-04 16:51:57+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (9, 'YDZK8LC', 'tbl_module', 1, 'description', 'TESTING', NULL, 'update', 1, '2023-08-04 16:52:02+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (10, '26W61SD', 'tbl_module', 1, 'status', '1', '0', 'update', 1, '2023-08-04 16:52:06+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (11, 'X5MDSEZ', 'tbl_module', 1, 'status', '0', '1', 'update', 1, '2023-08-04 16:52:08+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (12, 'UZQZLWB', 'tbl_module', 2, 'all', NULL, NULL, 'create', 1, '2023-08-04 16:54:26+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (13, 'GWY06BI', 'tbl_module', 3, 'all', NULL, NULL, 'create', 1, '2023-08-04 16:55:09+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (14, '3UKOM4X', 'tbl_module', 4, 'all', NULL, NULL, 'create', 1, '2023-08-04 16:55:19+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (15, '1AURXEL', 'tbl_module', 4, 'status', '1', '0', 'update', 1, '2023-08-07 08:37:02+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (16, 'CTP4SJN', 'tbl_module', 4, 'status', '0', '1', 'update', 1, '2023-08-07 08:37:07+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (17, '3YWWB7K', 'tbl_module', 4, 'status', '1', '0', 'update', 1, '2023-08-07 10:19:17+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (18, 'UIVISJH', 'tbl_module', 4, 'status', '0', '1', 'update', 1, '2023-08-07 10:19:27+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (19, 'XJCWPI6', 'tbl_sub_module', 1, 'all', NULL, NULL, 'create', 1, '2023-08-07 15:00:37+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (20, 'X0PHH1E', 'tbl_sub_module', 2, 'all', NULL, NULL, 'create', 1, '2023-08-07 15:40:31+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (21, 'Q7C5IRE', 'tbl_sub_module', 3, 'all', NULL, NULL, 'create', 1, '2023-08-07 15:53:55+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (22, 'PY0QM4G', 'tbl_sub_module', 4, 'all', NULL, NULL, 'create', 1, '2023-08-07 16:00:12+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (23, 'W884MBE', 'tbl_module', 4, 'status', '1', '0', 'update', 1, '2023-08-07 16:21:07+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (24, '83NCSJY', 'tbl_module', 4, 'status', '0', '1', 'update', 1, '2023-08-07 16:21:15+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (25, 'HUHIKVW', 'tbl_module', 4, 'status', '1', '0', 'update', 1, '2023-08-09 15:39:08+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (26, 'S7T22JL', 'tbl_module', 4, 'status', '0', '1', 'update', 1, '2023-08-09 15:39:11+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (27, 'V8EF1N4', 'tbl_sub_module', 4, 'status', '1', '0', 'update', 1, '2023-08-10 16:34:42+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (28, '4L2MQW4', 'tbl_sub_module', 4, 'status', '0', '1', 'update', 1, '2023-08-10 16:34:46+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (29, 'ULCNWOX', 'tbl_company', 2, 'address', '#82 CORDILLERA ST. CORNER QUEZON AVE., QUEZON CITY', NULL, 'update', 1, '2023-08-14 09:27:49+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (30, 'XWKAM3C', 'tbl_company', 4, 'all', NULL, NULL, 'create', 1, '2023-08-14 11:26:04+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (31, 'TVK1MFJ', 'tbl_department', 1, 'all', NULL, NULL, 'create', 1, '2023-08-14 11:29:03+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (32, 'S5N0Y9G', 'tbl_department', 2, 'all', NULL, NULL, 'create', 1, '2023-08-14 11:40:31+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (33, 'KTK1JXQ', 'tbl_department', 3, 'all', NULL, NULL, 'create', 1, '2023-08-14 11:40:53+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (34, 'HGKYPOG', 'tbl_department', 4, 'all', NULL, NULL, 'create', 1, '2023-08-14 11:41:01+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (35, '3E2JW11', 'tbl_company', 4, 'status', '1', '0', 'update', 1, '2023-08-14 11:56:42+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (36, '7PV5MYN', 'tbl_company', 4, 'status', '0', '1', 'update', 1, '2023-08-14 11:56:50+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (37, 'O1S9IET', 'tbl_department', 4, 'status', '1', '0', 'update', 1, '2023-08-14 11:57:16+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (38, 'X67OKPX', 'tbl_department', 4, 'status', '0', '1', 'update', 1, '2023-08-14 11:57:19+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (39, 'I13IHMP', 'tbl_position', 1, 'all', NULL, NULL, 'create', 1, '2023-08-14 14:40:08+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (40, '07772PO', 'tbl_position', 2, 'all', NULL, NULL, 'create', 1, '2023-08-14 14:40:29+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (41, '8XV90RU', 'tbl_position', 3, 'all', NULL, NULL, 'create', 1, '2023-08-14 14:41:53+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (42, '25D4UZK', 'tbl_department', 3, 'status', '1', '0', 'update', 1, '2023-08-14 14:58:39+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (43, 'VZPMR1Y', 'tbl_position', 3, 'status', '0', '1', 'update', 1, '2023-08-14 14:58:59+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (44, '11HNU8B', 'tbl_position', 3, 'status', '1', '0', 'update', 1, '2023-08-14 14:59:02+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (45, '3A36YCQ', 'tbl_position', 3, 'status', '0', '1', 'update', 1, '2023-08-14 14:59:06+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (46, 'YY7AETT', 'tbl_sub_module', 5, 'all', NULL, NULL, 'create', 1, '2023-08-14 15:07:56+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (47, 'P5MH0QM', 'tbl_users', 2, 'all', NULL, NULL, 'create', 1, '2023-08-15 16:45:58+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (48, 'MP14657', 'tbl_position', 4, 'all', NULL, NULL, 'create', 1, '2023-08-16 09:01:02+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (49, 'OU90ZRQ', 'tbl_users', 2, 'all', NULL, NULL, 'create', 1, '2023-08-16 09:48:47+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (50, '4MIOSV4', 'tbl_users', 3, 'all', NULL, NULL, 'create', 1, '2023-08-16 10:35:22+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (51, 'NG0ULDQ', 'tbl_users', 3, 'status', '1', '0', 'update', 1, '2023-08-16 14:33:29+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (52, 'M4HELQW', 'tbl_users', 3, 'status', '0', '1', 'update', 1, '2023-08-16 14:33:36+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (53, 'JBQ2OBT', 'tbl_assets_supplies_brand', 1, 'all', NULL, NULL, 'create', 1, '2023-08-16 16:53:32+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (54, 'GAGOGKD', 'tbl_assets_supplies_brand', 2, 'all', NULL, NULL, 'create', 1, '2023-08-16 16:54:28+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (55, 'KS5FE8E', 'tbl_assets_supplies_brand', 2, 'status', '1', '0', 'update', 1, '2023-08-17 09:05:42+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (56, 'KO3I2CI', 'tbl_assets_supplies_brand', 2, 'status', '0', '1', 'update', 1, '2023-08-17 09:05:46+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (57, '0T7W429', 'tbl_sub_module', 6, 'all', NULL, NULL, 'create', 1, '2023-08-17 10:24:10+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (58, 'YXF87W5', 'tbl_assets_supplies_classification', 1, 'all', NULL, NULL, 'create', 1, '2023-08-17 15:25:14+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (59, 'LTW1MS8', 'tbl_assets_supplies_classification', 2, 'all', NULL, NULL, 'create', 1, '2023-08-17 15:27:46+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (60, '6JNL8WM', 'tbl_assets_supplies_classification', 2, 'status', '1', '0', 'update', 1, '2023-08-17 15:44:07+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (61, 'TK3J7CR', 'tbl_assets_supplies_classification', 2, 'status', '0', '1', 'update', 1, '2023-08-17 15:44:10+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (62, 'YMAD4S3', 'tbl_sub_module', 7, 'all', NULL, NULL, 'create', 1, '2023-08-17 16:27:58+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (63, 'ZRGJ0Y0', 'tbl_sub_module', 8, 'all', NULL, NULL, 'create', 1, '2023-08-17 16:28:06+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (64, '5OBX35V', 'tbl_assets_supplies_brand', 3, 'all', NULL, NULL, 'create', 1, '2023-08-24 16:17:27+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (65, '4EULG9T', 'tbl_assets_supplies_brand', 4, 'all', NULL, NULL, 'create', 1, '2023-08-24 16:17:53+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (66, 'LXWOVIR', 'tbl_assets_supplies_brand', 5, 'all', NULL, NULL, 'create', 1, '2023-08-24 16:19:31+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (67, 'K38164S', 'tbl_assets_supplies_classification', 3, 'all', NULL, NULL, 'create', 1, '2023-08-24 16:19:41+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (68, 'UXI28CT', 'tbl_brands', 1, 'all', NULL, NULL, 'create', 1, '2023-08-25 10:27:27+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (69, '1GRYF4R', 'tbl_brands', 2, 'all', NULL, NULL, 'create', 1, '2023-08-25 10:29:01+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (70, 'HWIOERQ', 'tbl_brands', 3, 'all', NULL, NULL, 'create', 1, '2023-08-25 10:29:28+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (71, 'RCPZ3X9', 'tbl_brands', 4, 'all', NULL, NULL, 'create', 1, '2023-08-25 10:29:37+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (72, 'IGAN13L', 'tbl_brands', 4, 'status', '1', '0', 'update', 1, '2023-08-25 10:32:07+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (73, 'MWDL6EZ', 'tbl_brands', 4, 'status', '0', '1', 'update', 1, '2023-08-25 10:32:11+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (74, '0GN8C48', 'tbl_brands', 5, 'all', NULL, NULL, 'create', 1, '2023-08-25 16:13:38+08');
-INSERT INTO "public"."tbl_audit_trail" VALUES (75, 'M7E679Z', 'tbl_brands', 6, 'all', NULL, NULL, 'create', 1, '2023-08-25 16:13:48+08');
 
 -- ----------------------------
 -- Table structure for tbl_brands
@@ -278,6 +250,7 @@ INSERT INTO "public"."tbl_brands" VALUES (3, 'BRD-0000003', 'assets', 'system_un
 INSERT INTO "public"."tbl_brands" VALUES (4, 'BRD-0000004', 'assets', 'system_unit', 'HP', 1, 1, 1, NULL, NULL, '2023-08-25 10:29:37+08', '2023-08-25 10:32:11+08', NULL, NULL);
 INSERT INTO "public"."tbl_brands" VALUES (5, 'BRD-0000005', 'supplies', 'office_chair', 'OFX', 1, 1, NULL, NULL, NULL, '2023-08-25 16:13:38+08', NULL, NULL, NULL);
 INSERT INTO "public"."tbl_brands" VALUES (6, 'BRD-0000006', 'supplies', 'table', 'OFX', 1, 1, NULL, NULL, NULL, '2023-08-25 16:13:48+08', NULL, NULL, NULL);
+INSERT INTO "public"."tbl_brands" VALUES (8, 'BRD-0000007', 'assets', 'laptop', 'ACER', 1, 1, NULL, NULL, NULL, '2023-08-28 00:07:38+08', NULL, NULL, NULL);
 
 -- ----------------------------
 -- Table structure for tbl_company
@@ -496,75 +469,87 @@ INSERT INTO "public"."tbl_users" VALUES (3, 'USR-0000001', 'calvs23@gmail.com', 
 -- ----------------------------
 ALTER SEQUENCE "public"."tbl_assets_id_seq"
 OWNED BY "public"."tbl_assets"."id";
-SELECT setval('"public"."tbl_assets_id_seq"', 2, false);
+SELECT setval('"public"."tbl_assets_id_seq"', 3, false);
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."tbl_assets_info_id_seq"
+OWNED BY "public"."tbl_assets_info"."id";
+SELECT setval('"public"."tbl_assets_info_id_seq"', 2, false);
 
 -- ----------------------------
 -- Alter sequences owned by
 -- ----------------------------
 ALTER SEQUENCE "public"."tbl_audit_trail_id_seq"
 OWNED BY "public"."tbl_audit_trail"."id";
-SELECT setval('"public"."tbl_audit_trail_id_seq"', 76, true);
+SELECT setval('"public"."tbl_audit_trail_id_seq"', 2, false);
 
 -- ----------------------------
 -- Alter sequences owned by
 -- ----------------------------
 ALTER SEQUENCE "public"."tbl_brands_id_seq"
 OWNED BY "public"."tbl_brands"."id";
-SELECT setval('"public"."tbl_brands_id_seq"', 7, true);
+SELECT setval('"public"."tbl_brands_id_seq"', 9, true);
 
 -- ----------------------------
 -- Alter sequences owned by
 -- ----------------------------
 ALTER SEQUENCE "public"."tbl_company_id_seq"
 OWNED BY "public"."tbl_company"."id";
-SELECT setval('"public"."tbl_company_id_seq"', 5, true);
+SELECT setval('"public"."tbl_company_id_seq"', 6, true);
 
 -- ----------------------------
 -- Alter sequences owned by
 -- ----------------------------
 ALTER SEQUENCE "public"."tbl_department_id_seq"
 OWNED BY "public"."tbl_department"."id";
-SELECT setval('"public"."tbl_department_id_seq"', 5, true);
+SELECT setval('"public"."tbl_department_id_seq"', 6, true);
 
 -- ----------------------------
 -- Alter sequences owned by
 -- ----------------------------
 ALTER SEQUENCE "public"."tbl_employee_id_seq"
 OWNED BY "public"."tbl_employee"."id";
-SELECT setval('"public"."tbl_employee_id_seq"', 4, true);
+SELECT setval('"public"."tbl_employee_id_seq"', 5, true);
 
 -- ----------------------------
 -- Alter sequences owned by
 -- ----------------------------
 ALTER SEQUENCE "public"."tbl_module_id_seq"
 OWNED BY "public"."tbl_module"."id";
-SELECT setval('"public"."tbl_module_id_seq"', 5, true);
+SELECT setval('"public"."tbl_module_id_seq"', 6, true);
 
 -- ----------------------------
 -- Alter sequences owned by
 -- ----------------------------
 ALTER SEQUENCE "public"."tbl_position_id_seq"
 OWNED BY "public"."tbl_position"."id";
-SELECT setval('"public"."tbl_position_id_seq"', 5, true);
+SELECT setval('"public"."tbl_position_id_seq"', 6, true);
 
 -- ----------------------------
 -- Alter sequences owned by
 -- ----------------------------
 ALTER SEQUENCE "public"."tbl_sub_module_id_seq"
 OWNED BY "public"."tbl_sub_module"."id";
-SELECT setval('"public"."tbl_sub_module_id_seq"', 8, true);
+SELECT setval('"public"."tbl_sub_module_id_seq"', 9, true);
 
 -- ----------------------------
 -- Alter sequences owned by
 -- ----------------------------
 ALTER SEQUENCE "public"."tbl_users_id_seq"
 OWNED BY "public"."tbl_users"."id";
-SELECT setval('"public"."tbl_users_id_seq"', 4, true);
+SELECT setval('"public"."tbl_users_id_seq"', 5, true);
 
 -- ----------------------------
 -- Primary Key structure for table tbl_assets
 -- ----------------------------
 ALTER TABLE "public"."tbl_assets" ADD CONSTRAINT "tbl_assets_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Primary Key structure for table tbl_assets_info
+-- ----------------------------
+ALTER TABLE "public"."tbl_assets_info" ADD CONSTRAINT "tbl_assets_info_pkey" PRIMARY KEY ("id");
 
 -- ----------------------------
 -- Primary Key structure for table tbl_audit_trail
@@ -618,6 +603,12 @@ ALTER TABLE "public"."tbl_assets" ADD CONSTRAINT "tbl_assets_created_by_fkey" FO
 ALTER TABLE "public"."tbl_assets" ADD CONSTRAINT "tbl_assets_deleted_by_fkey" FOREIGN KEY ("deleted_by") REFERENCES "public"."tbl_users" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
 ALTER TABLE "public"."tbl_assets" ADD CONSTRAINT "tbl_assets_imported_by_fkey" FOREIGN KEY ("imported_by") REFERENCES "public"."tbl_users" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
 ALTER TABLE "public"."tbl_assets" ADD CONSTRAINT "tbl_assets_updated_by_fkey" FOREIGN KEY ("updated_by") REFERENCES "public"."tbl_users" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- ----------------------------
+-- Foreign Keys structure for table tbl_assets_info
+-- ----------------------------
+ALTER TABLE "public"."tbl_assets_info" ADD CONSTRAINT "tbl_assets_info_assets_id_fkey" FOREIGN KEY ("assets_id") REFERENCES "public"."tbl_assets" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "public"."tbl_assets_info" ADD CONSTRAINT "tbl_assets_info_brand_id_fkey" FOREIGN KEY ("brand_id") REFERENCES "public"."tbl_brands" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- ----------------------------
 -- Foreign Keys structure for table tbl_audit_trail
