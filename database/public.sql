b9ca0c90b13258ca2744197ba2c2fccea2e087bf
Pijay <53463564+yopijay@users.noreply.github.com>
2023-09-03 23:24:41 +0800

    Update public.sql

--- database/public.sql
+++ database/public.sql
@@ -1,18 +1,18 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : kc
+ Source Server         : local
  Source Server Type    : PostgreSQL
- Source Server Version : 140003
+ Source Server Version : 150001
  Source Host           : localhost:5432
- Source Catalog        : db_kc_ams
+ Source Catalog        : db_ams
  Source Schema         : public
 
  Target Server Type    : PostgreSQL
- Target Server Version : 140003
+ Target Server Version : 150001
  File Encoding         : 65001
 
- Date: 29/08/2023 12:00:28
+ Date: 03/09/2023 23:24:34
 */
 
 
@@ -127,6 +127,28 @@ START 1
 CACHE 1;
 
 -- ----------------------------
+-- Sequence structure for tbl_supplies_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."tbl_supplies_id_seq";
+CREATE SEQUENCE "public"."tbl_supplies_id_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 2147483647
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for tbl_supplies_info_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."tbl_supplies_info_id_seq";
+CREATE SEQUENCE "public"."tbl_supplies_info_id_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 2147483647
+START 1
+CACHE 1;
+
+-- ----------------------------
 -- Sequence structure for tbl_users_id_seq
 -- ----------------------------
 DROP SEQUENCE IF EXISTS "public"."tbl_users_id_seq";
@@ -223,6 +245,9 @@ CREATE TABLE "public"."tbl_audit_trail" (
 INSERT INTO "public"."tbl_audit_trail" VALUES (2, 'DBBJ0T4', 'tbl_brands', 3, 'all', NULL, NULL, 'create', 1, '2023-08-29 10:20:09+08');
 INSERT INTO "public"."tbl_audit_trail" VALUES (4, '8Y3YZZB', 'tbl_brands', 10, 'all', NULL, NULL, 'create', 1, '2023-08-29 10:51:41+08');
 INSERT INTO "public"."tbl_audit_trail" VALUES (3, 'NNARS0K', 'tbl_assets', 1, 'all', NULL, NULL, 'create', 1, '2023-08-29 10:26:09+08');
+INSERT INTO "public"."tbl_audit_trail" VALUES (6, 'GBRGER4', 'tbl_brands', 12, 'all', NULL, NULL, 'create', 1, '2023-08-31 13:52:48+08');
+INSERT INTO "public"."tbl_audit_trail" VALUES (7, '7J6Y5DF', 'tbl_brands', 13, 'all', NULL, NULL, 'create', 1, '2023-08-31 13:52:58+08');
+INSERT INTO "public"."tbl_audit_trail" VALUES (8, 'AJ6RXUK', 'tbl_brands', 14, 'all', NULL, NULL, 'create', 1, '2023-08-31 13:53:02+08');
 
 -- ----------------------------
 -- Table structure for tbl_brands
@@ -257,6 +282,9 @@ INSERT INTO "public"."tbl_brands" VALUES (5, 'BRD-0000005', 'supplies', 'office_
 INSERT INTO "public"."tbl_brands" VALUES (6, 'BRD-0000006', 'supplies', 'table', 'OFX', 1, 1, NULL, NULL, NULL, '2023-08-25 16:13:48+08', NULL, NULL, NULL);
 INSERT INTO "public"."tbl_brands" VALUES (8, 'BRD-0000007', 'assets', 'laptop', 'ACER', 1, 1, NULL, NULL, NULL, '2023-08-28 00:07:38+08', NULL, NULL, NULL);
 INSERT INTO "public"."tbl_brands" VALUES (10, 'BRD-0000008', 'assets', 'laptop', 'DELL', 1, 1, NULL, NULL, NULL, '2023-08-29 10:51:41+08', NULL, NULL, NULL);
+INSERT INTO "public"."tbl_brands" VALUES (12, 'BRD-0000009', 'supplies', 'toner', 'ABOC', 1, 1, NULL, NULL, NULL, '2023-08-31 13:52:48+08', NULL, NULL, NULL);
+INSERT INTO "public"."tbl_brands" VALUES (13, 'BRD-0000010', 'supplies', 'toner', 'SAMSUNG', 1, 1, NULL, NULL, NULL, '2023-08-31 13:52:58+08', NULL, NULL, NULL);
+INSERT INTO "public"."tbl_brands" VALUES (14, 'BRD-0000011', 'supplies', 'toner', 'HP', 1, 1, NULL, NULL, NULL, '2023-08-31 13:53:02+08', NULL, NULL, NULL);
 
 -- ----------------------------
 -- Table structure for tbl_company
@@ -442,6 +470,47 @@ INSERT INTO "public"."tbl_sub_module" VALUES (6, 'SMDL-0000007', 2, 'ASSETS', 'a
 INSERT INTO "public"."tbl_sub_module" VALUES (7, 'SMDL-0000008', 2, 'SUPPLIES', 'supplies', 1, 1, NULL, NULL, NULL, '2023-08-17 16:28:06+08', NULL, NULL, NULL);
 
 -- ----------------------------
+-- Table structure for tbl_supplies
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."tbl_supplies";
+CREATE TABLE "public"."tbl_supplies" (
+  "id" int4 NOT NULL DEFAULT nextval('tbl_supplies_id_seq'::regclass),
+  "series_no" varchar(20) COLLATE "pg_catalog"."default",
+  "type" varchar(50) COLLATE "pg_catalog"."default",
+  "brand_id" int4,
+  "name" varchar(100) COLLATE "pg_catalog"."default",
+  "status" int4,
+  "created_by" int4,
+  "updated_by" int4,
+  "deleted_by" int4,
+  "imported_by" int4,
+  "date_created" timestamptz(6),
+  "date_updated" timestamptz(6),
+  "date_deleted" timestamptz(6),
+  "date_imported" timestamptz(6)
+)
+;
+
+-- ----------------------------
+-- Records of tbl_supplies
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tbl_supplies_info
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."tbl_supplies_info";
+CREATE TABLE "public"."tbl_supplies_info" (
+  "id" int4 NOT NULL DEFAULT nextval('tbl_supplies_info_id_seq'::regclass),
+  "supplies_id" int4,
+  "quantity" int4
+)
+;
+
+-- ----------------------------
+-- Records of tbl_supplies_info
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for tbl_users
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."tbl_users";
@@ -475,77 +544,91 @@ INSERT INTO "public"."tbl_users" VALUES (3, 'USR-0000001', 'calvs23@gmail.com', 
 -- ----------------------------
 ALTER SEQUENCE "public"."tbl_assets_id_seq"
 OWNED BY "public"."tbl_assets"."id";
-SELECT setval('"public"."tbl_assets_id_seq"', 2, true);
+SELECT setval('"public"."tbl_assets_id_seq"', 3, true);
 
 -- ----------------------------
 -- Alter sequences owned by
 -- ----------------------------
 ALTER SEQUENCE "public"."tbl_assets_info_id_seq"
 OWNED BY "public"."tbl_assets_info"."id";
-SELECT setval('"public"."tbl_assets_info_id_seq"', 2, true);
+SELECT setval('"public"."tbl_assets_info_id_seq"', 3, true);
 
 -- ----------------------------
 -- Alter sequences owned by
 -- ----------------------------
 ALTER SEQUENCE "public"."tbl_audit_trail_id_seq"
 OWNED BY "public"."tbl_audit_trail"."id";
-SELECT setval('"public"."tbl_audit_trail_id_seq"', 5, true);
+SELECT setval('"public"."tbl_audit_trail_id_seq"', 9, true);
 
 -- ----------------------------
 -- Alter sequences owned by
 -- ----------------------------
 ALTER SEQUENCE "public"."tbl_brands_id_seq"
 OWNED BY "public"."tbl_brands"."id";
-SELECT setval('"public"."tbl_brands_id_seq"', 11, true);
+SELECT setval('"public"."tbl_brands_id_seq"', 15, true);
 
 -- ----------------------------
 -- Alter sequences owned by
 -- ----------------------------
 ALTER SEQUENCE "public"."tbl_company_id_seq"
 OWNED BY "public"."tbl_company"."id";
-SELECT setval('"public"."tbl_company_id_seq"', 7, true);
+SELECT setval('"public"."tbl_company_id_seq"', 8, true);
 
 -- ----------------------------
 -- Alter sequences owned by
 -- ----------------------------
 ALTER SEQUENCE "public"."tbl_department_id_seq"
 OWNED BY "public"."tbl_department"."id";
-SELECT setval('"public"."tbl_department_id_seq"', 7, true);
+SELECT setval('"public"."tbl_department_id_seq"', 8, true);
 
 -- ----------------------------
 -- Alter sequences owned by
 -- ----------------------------
 ALTER SEQUENCE "public"."tbl_employee_id_seq"
 OWNED BY "public"."tbl_employee"."id";
-SELECT setval('"public"."tbl_employee_id_seq"', 6, true);
+SELECT setval('"public"."tbl_employee_id_seq"', 7, true);
 
 -- ----------------------------
 -- Alter sequences owned by
 -- ----------------------------
 ALTER SEQUENCE "public"."tbl_module_id_seq"
 OWNED BY "public"."tbl_module"."id";
-SELECT setval('"public"."tbl_module_id_seq"', 7, true);
+SELECT setval('"public"."tbl_module_id_seq"', 8, true);
 
 -- ----------------------------
 -- Alter sequences owned by
 -- ----------------------------
 ALTER SEQUENCE "public"."tbl_position_id_seq"
 OWNED BY "public"."tbl_position"."id";
-SELECT setval('"public"."tbl_position_id_seq"', 7, true);
+SELECT setval('"public"."tbl_position_id_seq"', 8, true);
 
 -- ----------------------------
 -- Alter sequences owned by
 -- ----------------------------
 ALTER SEQUENCE "public"."tbl_sub_module_id_seq"
 OWNED BY "public"."tbl_sub_module"."id";
-SELECT setval('"public"."tbl_sub_module_id_seq"', 10, true);
+SELECT setval('"public"."tbl_sub_module_id_seq"', 11, true);
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."tbl_supplies_id_seq"
+OWNED BY "public"."tbl_supplies"."id";
+SELECT setval('"public"."tbl_supplies_id_seq"', 2, false);
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."tbl_supplies_info_id_seq"
+OWNED BY "public"."tbl_supplies_info"."id";
+SELECT setval('"public"."tbl_supplies_info_id_seq"', 2, false);
 
 -- ----------------------------
 -- Alter sequences owned by
 -- ----------------------------
 ALTER SEQUENCE "public"."tbl_users_id_seq"
 OWNED BY "public"."tbl_users"."id";
-SELECT setval('"public"."tbl_users_id_seq"', 6, true);
+SELECT setval('"public"."tbl_users_id_seq"', 7, true);
 
 -- ----------------------------
 -- Primary Key structure for table tbl_assets
@@ -596,6 +679,16 @@ ALTER TABLE "public"."tbl_position" ADD CONSTRAINT "tbl_position_pkey" PRIMARY K
 -- Primary Key structure for table tbl_sub_module
 -- ----------------------------
 ALTER TABLE "public"."tbl_sub_module" ADD CONSTRAINT "tbl_sub_module_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Primary Key structure for table tbl_supplies
+-- ----------------------------
+ALTER TABLE "public"."tbl_supplies" ADD CONSTRAINT "tbl_supplies_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Primary Key structure for table tbl_supplies_info
+-- ----------------------------
+ALTER TABLE "public"."tbl_supplies_info" ADD CONSTRAINT "tbl_supplies_info_pkey" PRIMARY KEY ("id");
 
 -- ----------------------------
 -- Primary Key structure for table tbl_users
@@ -677,6 +770,20 @@ ALTER TABLE "public"."tbl_sub_module" ADD CONSTRAINT "tbl_sub_module_deleted_by_
 ALTER TABLE "public"."tbl_sub_module" ADD CONSTRAINT "tbl_sub_module_imported_by_fkey" FOREIGN KEY ("imported_by") REFERENCES "public"."tbl_users" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
 ALTER TABLE "public"."tbl_sub_module" ADD CONSTRAINT "tbl_sub_module_module_id_fkey" FOREIGN KEY ("module_id") REFERENCES "public"."tbl_module" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
 ALTER TABLE "public"."tbl_sub_module" ADD CONSTRAINT "tbl_sub_module_updated_by_fkey" FOREIGN KEY ("updated_by") REFERENCES "public"."tbl_users" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- ----------------------------
+-- Foreign Keys structure for table tbl_supplies
+-- ----------------------------
+ALTER TABLE "public"."tbl_supplies" ADD CONSTRAINT "tbl_supplies_brand_id_fkey" FOREIGN KEY ("brand_id") REFERENCES "public"."tbl_brands" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "public"."tbl_supplies" ADD CONSTRAINT "tbl_supplies_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "public"."tbl_users" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "public"."tbl_supplies" ADD CONSTRAINT "tbl_supplies_deleted_by_fkey" FOREIGN KEY ("deleted_by") REFERENCES "public"."tbl_users" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "public"."tbl_supplies" ADD CONSTRAINT "tbl_supplies_imported_by_fkey" FOREIGN KEY ("imported_by") REFERENCES "public"."tbl_users" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "public"."tbl_supplies" ADD CONSTRAINT "tbl_supplies_updated_by_fkey" FOREIGN KEY ("updated_by") REFERENCES "public"."tbl_users" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- ----------------------------
+-- Foreign Keys structure for table tbl_supplies_info
+-- ----------------------------
+ALTER TABLE "public"."tbl_supplies_info" ADD CONSTRAINT "tbl_supplies_info_supplies_id_fkey" FOREIGN KEY ("supplies_id") REFERENCES "public"."tbl_supplies" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- ----------------------------
 -- Foreign Keys structure for table tbl_users
