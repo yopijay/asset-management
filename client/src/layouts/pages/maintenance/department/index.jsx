0aee0b845aa67906b99d9c3b96c801ca9d9bf3c6
Kuya Pijay <53463564+yopijay@users.noreply.github.com>
2023-12-13 09:21:59 +0800

    Department
    - Authorization
    - Feature permissions

--- client/src/layouts/pages/maintenance/department/index.jsx
+++ client/src/layouts/pages/maintenance/department/index.jsx
@@ -1,9 +1,10 @@
 // Libraries
 import { useContext, useEffect } from "react";
 import { Stack } from "@mui/material";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
 // Core
+import { AccountCntxt } from "core/context/Account"; // Context
 import { GlobalCntxt } from "core/context/Global"; // Context
 import { FormPrvdr } from "core/context/Form"; // Provider
 import { ListPrvdr } from "core/context/List"; // Provider
@@ -24,13 +25,18 @@ const container = {
 }
 
 const Index = () => {
+    const navigate = useNavigate();
     const { setactive } = useContext(GlobalCntxt);
+    const { data } = useContext(AccountCntxt);
 
     useEffect(() => {
-        document.title = 'GAMS | Department';
-        setactive('department');
-        localStorage.setItem('nav', 'department');
-    }, [ setactive ]);
+        if(data.user_level !== 'superadmin' && (data.permission === null || !JSON.parse(data.permission).maintenance.department.list)) { navigate('/'); }
+        else {
+            document.title = 'GAMS | Department';
+            setactive('department');
+            localStorage.setItem('nav', 'department');
+        }
+    }, [ data, navigate, setactive ]);
 
     return (
         <Stack direction= "row" justifyContent= "flex-start" alignItems= "flex-start" sx= { container }>
